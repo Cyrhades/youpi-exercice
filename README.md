433436579903b42b935b9d739f51d4851999ce4c
Max <masamune.code@gmail.com>
2019-08-23 13:35:08 +0200

    ADD: Basic HMR

--- README.md
+++ README.md
@@ -11,6 +11,75 @@ gulp / gulp-cli // accès à gulp
 yarn // Alternative à npm, plus rapide
 
 
+#### HMR
+Browser sync package
+https://www.browsersync.io/docs/gulp
+
+#### Html linter
+Gulp package
+https://www.npmjs.com/package/gulp-htmllint
+
+Original lib
+https://github.com/htmllint/htmllint/
+
+Config options
+https://github.com/htmllint/htmllint/wiki/Options
+
+#### Css linter
+Gulp package
+https://www.npmjs.com/package/gulp-stylelint
+
+Original lib
+https://stylelint.io/
+
+Config options
+https://www.npmjs.com/package/gulp-stylelint / https://stylelint.io/user-guide/configuration
+Possibilité d'autofix
+
+Existing config
+https://www.npmjs.com/package/stylelint-config-airbnb
+
+Fix deprecated dependencies, add stylelint-scss & stylelint-order
+
+#### JS linter
+Gulp package
+https://www.npmjs.com/package/gulp-eslint
+Possibilité de fix auto
+
+Original lib
+https://eslint.org/
+
+Existing config
+https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb
+
+Use without react
+https://github.com/airbnb/javascript/issues/451#issuecomment-133566960
+
+Copy file if all is ok
+https://github.com/lukeapage/gulp-eslint-if-fixed
+
+Jest compatibility
+https://stackoverflow.com/questions/44479207/prevent-test-expect-etc-is-not-defined-errors-when-using-jest/44479250
+
+#### JS UT / Jest
+Gulp packages are cancer
+// https://www.npmjs.com/package/gulp-jester
+// https://www.npmjs.com/package/gulp-jester-cli
+
+Original lib
+https://jestjs.io/
+
+CLI
+https://jestjs.io/docs/en/cli
+
+Config
+https://jestjs.io/docs/en/configuration
+
+Set up through package.json scripts, or directly call jest-cli.runCLI in gulpfile
+
+// https://medium.com/@ryanblahnik/setting-up-testing-with-jest-and-node-js-b793f1b5621e
+
+
 ### Others
 Editor Config
 https://editorconfig.org/
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
 
 // Plugins ressources
 const { dest, parallel, series, src, task, watch } 	= require('gulp'),
-  colors    = require('ansi-colors'),
-  fancyLog  = require('fancy-log'),
-  eslint    = require('gulp-eslint'),
-  htmllint  = require('gulp-htmllint'),
-  jestcli   = require('jest-cli'),
-  stylelint = require('gulp-stylelint')
+  colors      = require('ansi-colors'),
+  browserSync = require('browser-sync').create();
+  fancyLog    = require('fancy-log'),
+  eslint      = require('gulp-eslint'),
+  htmllint    = require('gulp-htmllint'),
+  jestcli     = require('jest-cli'),
+  stylelint   = require('gulp-stylelint')
 ;
 
 
@@ -18,6 +19,30 @@ const { dest, parallel, series, src, task, watch } 	= require('gulp'),
 task('copy-assets', function() {
 	return src(['src/**/*.*', '!node_modules/**'])
 		.pipe(dest('dist/'));
+});
+
+// Copy css & notify browser sync
+task('copy-css', function() {
+	return src('src/**/*.css')
+		.pipe(dest('dist/'))
+		.pipe(browserSync.stream());
+});
+
+// Copy html & notify browser sync
+task('copy-html', function() {
+	return src( 'src/**/*.html')
+		.pipe(dest('dist/'))
+		.pipe(browserSync.stream());
+});
+
+// Copy js & notify browser sync
+task('copy-js', function () {
+  return src( [
+    'src/**/*.js',
+    '!src/js/vendor/**/*.js'
+    ])
+		.pipe(dest('dist/'))
+		.pipe(browserSync.stream());
 });
 
 // Lint css & check for errors
@@ -46,6 +71,26 @@ task('lint-js', function() {
     .on('end', notifyJsIsOk );
 });
 
+// Create watcher
+task('start-watch', function () {
+  browserSync.init({
+      server: "./dist"
+  });
+
+  // Watch every tech
+	const watcherHTML = watch(['src/**/*.html']);
+	const watcherCSS 	= watch(['src/**/*.css']);
+	const watcherJS 	= watch([	'src/**/*.js',
+															'!src/js/vendor/**/*.js'
+														]);
+
+  // Update when necessary
+  watcherHTML	.on('change', parallel('copy-html'));
+  watcherCSS	.on('change', parallel('copy-css'));
+  watcherJS		.on('change', parallel('copy-js'));
+});
+
+
 // Unit test JS, via Jest CLI
 task('ut-js', () =>
 	jestcli.runCLI(
@@ -63,6 +108,9 @@ task('ut-js', () =>
 // Default tesk, executed when using 'gulp'
 //  	Capy all assets to dist
 task('default', parallel('copy-assets'));
+
+//  Watch : copy all files, and then update when necessary
+task('watch', series('default', 'start-watch'));
 
 // Run all linters & UT
 task('lint',    parallel('lint-html', 'lint-css', 'lint-js'));
